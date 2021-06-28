92da2cbb15f11b497c3983343ad4edeedf673417
bhhe <bowen.he.work@gmail.com>
2021-06-27 19:20:48 -0700

    Email row frontend complete

--- src/EmailList.js
+++ src/EmailList.js
@@ -12,6 +12,7 @@ import InboxIcon from '@material-ui/icons/Inbox';
 import PeopleIcon from '@material-ui/icons/People';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import Section from './Section';
+import EmailRow from "./EmailRow";
 
 function EmailList(){
     return (
@@ -48,6 +49,21 @@ function EmailList(){
                 <Section Icon={InboxIcon} title='Primary' color="red" selected />
                 <Section Icon={PeopleIcon} title='Social' color="#1A73E8"/>
                 <Section Icon={LocalOfferIcon} title='Promotions' color="green"/>
+            </div>
+
+            <div className="emailList__list">
+                <EmailRow
+                    title="Test"
+                    subject="test2"
+                    description="test3"
+                    time="10pm"
+                />
+                <EmailRow
+                    title="Test"
+                    subject="test2"
+                    description="test3"
+                    time="10pm"
+                />
             </div>
         </div>
     )
--- src/Mail.js
+++ src/Mail.js
@@ -4,7 +4,7 @@ import "./Mail.css";
 function Mail(){
     return (
         <div className='mail'>
-
+            Test Mail
         </div>
     )
 }
